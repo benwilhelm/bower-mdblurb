68fa9f1d18afccb1424e5960815f378254d564ce
Ben Wilhelm <bwilhelm@networkninja.com>
2014-10-17 15:21:14 -0500

    New blurb creation

--- mdblurb.js
+++ mdblurb.js
@@ -1,15 +1,15 @@
 (function($){
 
-	var $edit = $("<a href='#' class='fa fa-edit blurb-control-edit'></a>");
-	var $save = $("<a href='#' class='fa fa-save blurb-control-save'></a>");
-	var $cancel = $("<a href='#' class='fa fa-close blurb-control-cancel'></a>");
-	var $loading = $("<span class='fa fa-circle-o-notch fa-spin'></span>")
+	var $edit = function() { return $("<a href='#' class='fa fa-edit blurb-control-edit'></a>"); }
+	var $save = function() { return $("<a href='#' class='fa fa-save blurb-control-save'></a>"); }
+	var $cancel = function() { return $("<a href='#' class='fa fa-close blurb-control-cancel'></a>"); }
+	var $loading = function() { return $("<span class='fa fa-circle-o-notch fa-spin'></span>"); }
 
 	$.fn.addBlurbHandlers = function() {
 		var $blurb = $(this);
 		$blurb.on('click', '.blurb-control-edit', function(e){
 			e.preventDefault();
-			$(this).replaceWith($loading);
+			$(this).replaceWith($loading());
 			$blurb.blurbEditMode();
 		});
 
@@ -22,7 +22,7 @@
 
 		$blurb.on('click', '.blurb-control-save', function(e){
 			e.preventDefault();
-			$(this).replaceWith($loading);
+			$(this).replaceWith($loading());
 			$blurb.blurbSaveEdits();
 		});
 	}
@@ -31,24 +31,34 @@
 		var $this = $(this)
 		var $controls = $this.find('.blurb-controls').first();
 		var $content = $this.find('.blurb-content').first();
+		var $editor = $("<textarea class='blurb-editor'></textarea>")
 		$this.data('blurbContent', $content.html());
 		var w = $content.width();
 		var h = $content.height();
-		var url = '/blurb/' + $this.attr('data-blurb-id');
+		var dataId = $this.attr('data-blurb-id') || false;
 
-		$.getJSON(url, function(resp){
-			var markdown = resp.blurb.text;
-			var $editor = $("<textarea class='blurb-editor'></textarea>")
-			$editor.append(markdown || "&nbsp;");
+		if (dataId) {
+			var url = '/blurb/' + $this.attr('data-blurb-id');
+			$.getJSON(url, function(resp){
+				var markdown = resp.blurb.text;
+				$editor.append(markdown);
+				$editor.width(w);
+				$editor.height(h);
+				$content.html($editor);
+		
+				$controls.html('');
+				$controls.append($save());
+				$controls.append($cancel());
+
+			});
+		} else {
 			$editor.width(w);
-			$editor.height(h);
+			$editor.height(300);
 			$content.html($editor);
-	
 			$controls.html('');
-			$controls.append($save);
-			$controls.append($cancel);
-
-		});
+			$controls.append($save());
+			$controls.append($cancel());
+		}
 	}
 
 	$.fn.blurbCancelEdit = function() {
@@ -64,23 +74,30 @@
 		var $controls = $this.find('.blurb-controls').first();
 		var $content = $this.find('.blurb-content').first();
 		var $editor = $this.find('.blurb-editor').first();
-		var url = '/blurb/' + $this.attr('data-blurb-id');
 		var markdown = $editor.val();
-		console.log(markdown);
 
-		$.ajax( url, {
+		var ajaxSettings = {
 			type: 'POST',
 			data: {text: markdown},
-			headers: {
-				'X-HTTP-Method-Override': 'PUT'
-			},
-
 			success: function(resp) {
-				console.log(resp);
 				$content.html(resp.blurb.html);
 				$controls.html($edit);
+			}			
+		}
+
+		if ($this.attr('data-blurb-id')) {
+			var url = '/blurb/' + $this.attr('data-blurb-id');
+			ajaxSettings.headers = {
+				'X-HTTP-Method-Override': 'PUT'
 			}
-		})
+		} else {
+			var url = '/blurb'
+			ajaxSettings.headers = null
+			ajaxSettings.data.hash = $this.attr('id');
+			ajaxSettings.data.path = window.location.pathname;
+		}
+
+		$.ajax(url, ajaxSettings)
 	}
 
 
@@ -88,20 +105,21 @@
 		$('*[data-blurb=true]').each(function(idx, obj){
 			var $obj = $(obj);
 			var $spans = $obj.find('span[data-blurb-id]')
+
 			if ($spans.length) {
 				var $span = $spans.first();
-				$obj.attr('data-blurb-editable',true);
 				var dataId = $span.attr('data-blurb-id');
 				$obj.attr('data-blurb-id', dataId);
 				$span.remove();
-
-				$obj.wrapInner("<div class='blurb-content'></div>");
-				var $controls = $("<div class='blurb-controls'></div>")
-				$controls.append($edit);
-				$obj.append($controls);
-
-				$obj.addBlurbHandlers();
 			}
+
+			$obj.wrapInner("<div class='blurb-content'></div>");
+			var $controls = $("<div class='blurb-controls'></div>")
+			$controls.append($edit());
+			$obj.append($controls);
+
+			$obj.addBlurbHandlers();
+
 		})
 	})
 })(jQuery)
